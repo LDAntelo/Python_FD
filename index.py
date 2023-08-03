b5a7c5fce8d5f099a891a01d60eebdba67bb0ee6
LDAntelo <luisdiegoantelo0@gmail.com>
2023-08-03 14:30:44 -0300

    primer commit

---
+++ index.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/Contacto')
+def about():
+    return render_template('about.html')
+
+@app.route('/casupa')
+def casupa():
+    return render_template('casupa.html')
+
+@app.route('/informacion')
+def informacion():
+    return render_template('informacion.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
