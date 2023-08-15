2f66626769dd68ac4e9e9cecd756b2dd9873d750
LDAntelo <luisdiegoantelo0@gmail.com>
2023-08-14 22:38:14 -0300

    big data entry

--- index.py
+++ index.py
@@ -18,6 +18,30 @@ def casupa():
 def informacion():
     return render_template('informacion.html')
 
+@app.route('/grecco')
+def grecco():
+    return render_template('grecco.html')
+
+@app.route('/vergara')
+def vergara():
+    return render_template('vergara.html')
+
+@app.route('/nicobatlle')
+def nicobatlle():
+    return render_template('nicobatlle.html')
+
+@app.route('/corrales')
+def corrales():
+    return render_template('corrales.html')
+
+@app.route('/caraguata')
+def caraguata():
+    return render_template('caraguata.html')
+
+@app.route('/pruebascorrales')
+def pruebascorrales():
+    return render_template('pruebascorrales.html')
+
 if __name__ == '__main__':
     #app.run(debug=True)
     from waitress import serve
